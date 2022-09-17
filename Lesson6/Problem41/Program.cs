f44918ca09571f44459f5f2df113fd1784109224
Grigory Teleshev <Grigory.Teleshev@yandex.ru>
2022-09-17 19:29:11 +0300

    lesson 6 (first commit)

---
+++ Lesson6/Problem41/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3 */
+
+string errMess1 = "Число введено неверно";
+string firstMess = "Введите количество чисел для проверки (M): ";
+string secondMess = "Введите число ";
+string thirdMess = "Количество положительных чисел во введённой последовательности: ";
+
+CheckPositive(errMess1, firstMess, secondMess, thirdMess);
+
+void CheckPositive(string errorMessage, string firstMessage, string secondMessage, string thirdMessage)
+{
+    Console.Clear();
+    bool res1 = false;
+    bool res2 = false;
+    int M = 1;
+    int posCount = 0;
+    while (!res1)
+    {
+        Console.Write(firstMessage);
+        res1 = int.TryParse(Console.ReadLine(), out M);
+        if (!res1 == true)
+        {
+            Console.WriteLine(errorMessage);
+        }
+        else
+        {
+            int count = 0;
+            double[] numsToCheck = new double [M];
+            
+            while (!res2 || count < M)
+            {
+                Console.Write($"{secondMessage} {count + 1}: ");
+                res2 = double.TryParse(Console.ReadLine(), out numsToCheck[count]);
+                if (!res2 == true)
+                {
+                    Console.WriteLine(errorMessage);
+                }
+                else if(numsToCheck[count] > 0)
+                {
+                    posCount++;
+                    count++;
+                }
+                
+            }
+        }
+    }
+    Console.WriteLine(thirdMessage + posCount.ToString());
+}
+
---
+++ Lesson6/Problem43/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+/* Console.WriteLine("Hello, World!");
+ */
+
+double k1 = 1;
+double k2 = 3;
+double b1 = 4;
+double b2 = 6;
+
+double[] Intersection(double mult1, double constant1, double mult2,  double constant2)
+{
+    double[] intersectionCoord = new double [2];
+    string parallel = "Прямые параллельны \n";
+    if(mult1 == mult2)
+    {
+        Console.WriteLine(parallel);        
+    }
+    else
+    {
+        intersectionCoord[0] = (constant2 - constant1) / (mult1 - mult2);
+        intersectionCoord[1] = intersectionCoord[0] * mult1 + constant1;
+    }
+    return intersectionCoord;
+}
+
+double[] intCoord = Intersection(k1,b1,k2,b2);
+
+Console.WriteLine($"{intCoord[0]}, {intCoord[1]}");
