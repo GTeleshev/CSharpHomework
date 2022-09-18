cf52adc64b8f81109fd0c801d6e4fd660b35af7f
Grigory Teleshev <Grigory.Teleshev@yandex.ru>
2022-09-18 23:22:00 +0300

    lesson 6 final commit

--- Lesson6/Problem41/Program.cs
+++ Lesson6/Problem41/Program.cs
@@ -2,53 +2,64 @@
 0, 7, 8, -2, -2 -> 2
 -1, -7, 567, 89, 223-> 3 */
 
-string errMess1 = "Число введено неверно";
-string firstMess = "Введите количество чисел для проверки (M): ";
-string secondMess = "Введите число ";
-string thirdMess = "Количество положительных чисел во введённой последовательности: ";
+//объявление массива
+double[] array = new double[0];
+double[] newArray = InputNumberSequence(array);
+int cP = CountPositive(newArray);
+//вывод массива и результата
+Console.WriteLine($"Массив: [{String.Join(" ", newArray)}], количество положительных -> {cP}");
 
-CheckPositive(errMess1, firstMess, secondMess, thirdMess);
-
-void CheckPositive(string errorMessage, string firstMessage, string secondMessage, string thirdMessage)
+//метод ввода и обработки массива
+double[] InputNumberSequence(double[] arrayToFill)
 {
     Console.Clear();
     bool res1 = false;
     bool res2 = false;
-    int M = 1;
-    int posCount = 0;
-    while (!res1)
+    int M = -1;
+    string errorMessage = "Число введено неверно";
+    string firstMessage = "Введите количество чисел для проверки (M): ";
+    string secondMessage = "Введите число ";
+    while (!res1 || M < 0)
     {
         Console.Write(firstMessage);
         res1 = int.TryParse(Console.ReadLine(), out M);
-        if (!res1 == true)
+        if (!res1 == true || M < 0)
         {
             Console.WriteLine(errorMessage);
         }
         else
         {
             int count = 0;
-            double[] numsToCheck = new double [M];
-            
+            Array.Resize(ref arrayToFill, arrayToFill.Length + M);
             while (!res2 || count < M)
             {
                 Console.Write($"{secondMessage} {count + 1}: ");
-                res2 = double.TryParse(Console.ReadLine(), out numsToCheck[count]);
+                res2 = double.TryParse(Console.ReadLine(), out arrayToFill[count]);
                 if (!res2 == true)
                 {
                     Console.WriteLine(errorMessage);
-                }
-                if(numsToCheck[count] > 0)
-                {
-                    posCount++;
-                    count++;
                 }
                 else
                 {
                     count++;
                 }
-                
             }
+
         }
     }
-    Console.WriteLine(thirdMessage + posCount.ToString());
+    return arrayToFill;
+}
+//метод расчёт числа положительных
+int CountPositive(double[] arrayToCountPositive)
+{
+    int positiveCount = 0;
+    int len = arrayToCountPositive.Length;
+    for (int i = 0; i < len; i++)
+    {
+        if(arrayToCountPositive[i] > 0)
+        {
+            positiveCount++;
+        }
+    }
+    return positiveCount;
 }
--- Lesson6/Problem43/Program.cs
+++ Lesson6/Problem43/Program.cs
@@ -1,29 +1,61 @@
 ﻿/* Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 
-/* Console.WriteLine("Hello, World!");
- */
+double[,] linesParam = TwoLinesEquations();
+double[] intCoord = Intersection(linesParam);
 
-double k1 = 1;
-double k2 = 3;
-double b1 = 4;
-double b2 = 6;
-
-double[] intCoord = Intersection(k1,b1,k2,b2);
-Console.WriteLine($"{intCoord[0]}, {intCoord[1]}");
-
-double[] Intersection(double mult1, double constant1, double mult2,  double constant2)
+Console.WriteLine($"Координаты точки пересечения: ({intCoord[0]}; {intCoord[1]})");
+//вычисление точки пересечения
+double[] Intersection(double[,] linesParamArray) //mult1, double constant1, double mult2, double constant2
 {
-    double[] intersectionCoord = new double [2];
+    double[] intersectionCoord = new double[2]; //координаты точки пересечения
     string parallel = "Прямые параллельны \n";
-    if(mult1 == mult2)
+    if (linesParamArray[0, 0] == linesParamArray[1, 0]) //k1 == k2
     {
-        Console.WriteLine(parallel);        
+        Console.WriteLine(parallel);
     }
     else
     {
-        intersectionCoord[0] = (constant2 - constant1) / (mult1 - mult2);
-        intersectionCoord[1] = intersectionCoord[0] * mult1 + constant1;
+        intersectionCoord[0] = (linesParamArray[1, 1] - linesParamArray[0, 1]) /
+                                (linesParamArray[0, 0] - linesParamArray[1, 0]); //constant2 - constant1) / (mult1 - mult2)
+        intersectionCoord[1] = intersectionCoord[0] * linesParamArray[0, 0]
+                                + linesParamArray[0, 1]; //x *mult1 + constant1;
     }
     return intersectionCoord;
+}
+//параметры прямой
+double[,] TwoLinesEquations()
+{
+    Console.Clear();
+    double[] arrayToFill = new double[4];
+    Console.WriteLine("Параметры прямой: y = k * x + b (k - коэффициент, b - константа):");
+    bool res1 = false;
+    int M = arrayToFill.Length;
+    string errorMessage = "Число введено неверно";
+    string secondMessage = "Введите";
+    string[] prefixes = new string [4];
+    prefixes[0] = "k1";
+    prefixes[1] = "b1";
+    prefixes[2] = "k2";
+    prefixes[3] = "b2";
+    int count = 0;
+    while (!res1 || count < M)
+    {
+        Console.Write($"{secondMessage} {prefixes[count]}: ");
+        res1 = double.TryParse(Console.ReadLine(), out arrayToFill[count]);
+        if (!res1 == true)
+        {
+            Console.WriteLine(errorMessage);
+        }
+        else
+        {
+            count++;
+        }
+    }
+    double[,] outPutArray = new double[2,2];
+    outPutArray[0,0] = arrayToFill[0];
+    outPutArray[0,1] = arrayToFill[1];
+    outPutArray[1,0] = arrayToFill[2];
+    outPutArray[1,1] = arrayToFill[3];
+    return outPutArray;
 }
